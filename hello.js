1da50c8d94e6dbd676515d4a03d342e19872b67b
trieule50 <trieule50@gmail.com>
2021-11-15 15:52:11 -0500

    Add TS example

---
+++ hello.js
@@ -0,0 +1,11 @@
+// console.log("Hello world!");
+// Nothing happened - there were no type errors, so we didn’t get any output in our console since there was nothing to report. We got some JS file output instead.
+// This is an industrial-grade general-purpose greeter function:
+// function greet(person, date) {
+//     console.log(`Hello ${person}, today is ${date}!`);
+//   }
+//   greet("Brendan");
+function greet(person, date) {
+    console.log("Hello " + person + ", today is " + date.toDateString() + "!");
+}
+greet("Maddision", new Date());
